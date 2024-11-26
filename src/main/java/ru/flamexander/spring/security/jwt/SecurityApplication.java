dba3ff04480770242d3189a55618ba3e3a162a8c
stud <123@123>
2024-11-26 14:53:32 +0300

    123

---
+++ src/main/java/ru/flamexander/spring/security/jwt/SecurityApplication.java
@@ -0,0 +1,11 @@
+package ru.flamexander.spring.security.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+}
